7aa0663954474d60f4b49cbf78ceab86344fb8ca
Javkhlan <r.javkhlan@gmail.com>
2025-01-16 01:07:53 +0800

    category section implementation done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -102,65 +102,65 @@ class AdminController extends Controller
     public function category_store(Request $request){
         $request->validate([
             'name'=>'required',
-            'slug'=>'required|unique:brands,slug',
-            'image'=>'mimes::png,jpg,jpeg|max:2048'
+            'slug'=>'required|unique:categories,slug',
+            'image'=>'mimes::png,jpg,jpeg|max:2048',
         ]);
 
-        $brand = new Brand();
-        $brand->name = $request->name;
-        $brand->slug = Str::slug($request->name);
+        $category = new Category();
+        $category->name = $request->name;
+        $category->slug = Str::slug($request->name);
         $image = $request->file('image');
         $file_extention = $request->file('image')->extension();
-        $file_name = Carbon::now()->timestamp.'.'.$file_extention;
-        $this->GenerateBrandThumbnailsImage($image, $file_name);
-        $brand->image = $file_name;
-        $brand->save();
-        return redirect()->route('admin.brands')->with('status','Brand has been added succesfully');
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+        $this->GenerateCategoryThumbailImage($image,$file_name);
+        $category->image = $file_name;        
+        $category->save();
+        return redirect()->route('admin.categories')->with('status','Record has been added successfully !');
     }
-    public function brand_edit($id){
-        $brand = Brand::find($id);
-        return view('admin.brand-edit', compact('brand'));
+    public function category_edit($id){
+        $category = Category::find($id);
+        return view('admin.category-edit', compact('category'));
     }
-    public function brand_update(Request $request){
+    public function category_update(Request $request){
         $request->validate([
-            'name'=>'required',
-            'slug'=>'required|unique:brands,slug,'.$request->id,
-            'image'=>'mimes::png,jpg,jpeg|max:2048'
+            'name' => 'required',
+            'slug' => 'required|unique:categories,slug,'.$request->id,
+            'image' => 'mimes:png,jpg,jpeg|max:2048'
         ]);
 
-        $brand = Brand::find($request->id);
-        $brand->name = $request->name;
-        $brand->slug = Str::slug($request->name);
+        $category = Category::find($request->id);
+        $category->name = $request->name;
+        $category->slug = $request->slug;
         if($request->hasFile('image')){
-            if(File::exists(public_path('uploads/brands').'/'.$brand->image)){
-                File::delete(public_path('uploads/brands').'/'.$brand->image);
+            if(File::exists(public_path('uploads/categories').'/'.$category->image)){
+                File::delete(public_path('uploads/categories').'/'.$category->image);
             }
             $image = $request->file('image');
             $file_extention = $request->file('image')->extension();
             $file_name = Carbon::now()->timestamp.'.'.$file_extention;
-            $this->GenerateBrandThumbnailsImage($image, $file_name);
-            $brand->image = $file_name;
+            $this->GenerateCategoryThumbailImage($image, $file_name);
+            $category->image = $file_name;
         }
         
-        $brand->save();
-        return redirect()->route('admin.brands')->with('status','Brand has been updated succesfully');
-    }
-    public function GenerateBrandThumbnailsImage($image, $imageName){
-        $destinationPath = public_path('uploads/brands');
+        $category->save();
+        return redirect()->route('admin.categories')->with('status','Category has been updated succesfully');
+    } 
+    public function GenerateCategoryThumbailImage ($image, $imageName){
+        $destinationPath = public_path('uploads/categories');
         $img = Image::read($image->path());
         $img->cover(124,124,"top");
         $img->resize(124,124,function($constraint){
             $constraint->aspectRatio();
         })->save($destinationPath."/".$imageName);
     }
-    public function brand_delete($id)
+    public function category_delete($id)
     {
-        $brand = Brand::find($id);
-        if (File::exists(public_path('uploads/brands').'/'.$brand->image)) {
-            File::delete(public_path('uploads/brands').'/'.$brand->image);
+        $brand = Category::find($id);
+        if (File::exists(public_path('uploads/categories').'/'.$brand->image)) {
+            File::delete(public_path('uploads/categories').'/'.$brand->image);
         }
         $brand->delete();
-        return redirect()->route('admin.brands')->with('status','Brand has been deleted successfully !');
+        return redirect()->route('admin.categories')->with('status','Categories has been deleted successfully !');
     }
     
     //Category Related Admin Section Ends Here
