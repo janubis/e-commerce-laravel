af37659415ca0b95874b57da5b4e74f4df1ac240
Javkhlan <r.javkhlan@gmail.com>
2025-01-17 18:04:34 +0800

    top section sorting done

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -4,14 +4,45 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
-
+use App\Models\Brand;
+use App\Models\Category;
 class ShopController extends Controller
 {
     //
     public function index(Request $request)
     {        
-        $products = Product::orderBy('created_at','DESC')->paginate(12);
-        return view('shop',compact("products"));
+        $size = $request->query('size') ? $request->query('size') : 12;
+        
+        $o_column = "";
+        $o_order = "";
+        $order = $request->query("order") ? $request->query("order") : -1;
+        switch ($order){
+            case 1:
+                $o_column='created_at';
+                $o_order='DESC';
+                break;
+            case 2:
+                $o_column='created_at';
+                $o_order='ASC';
+                break;
+            case 3:
+                $o_column='sale_price';
+                $o_order='ASC';
+                break;
+            case 4:
+                $o_column='sale_price';
+                $o_order='DESC';
+                break;
+            default:
+                $o_column='id';
+                $o_order='DESC';
+                
+        }
+        $products = Product::orderBy($o_column, $o_order)->paginate($size);
+        $categories = Category::orderBy("name","ASC")->get();
+        $brands = Brand::orderBy("name","ASC")->get();
+
+        return view('shop',compact("products", 'size', 'order'));
     } 
     public function product_details($product_slug)
     {
